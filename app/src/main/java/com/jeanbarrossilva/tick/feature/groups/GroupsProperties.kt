b4f1d3bf44d47c474cb4629a33558a2baba33911
Jean Silva <jeanbarrossilva@outlook.com>
2023-05-18 11:39:25 -0300

    refact: remove GroupsProperties

--- app/src/main/java/com/jeanbarrossilva/tick/feature/groups/GroupsProperties.kt
+++
@@ -1,9 +0,0 @@
-package com.jeanbarrossilva.tick.feature.groups
-
-import com.jeanbarrossilva.tick.core.todo.domain.group.ToDoGroup
-import java.io.Serializable
-
-data class GroupsProperties(
-    val selectedGroup: ToDoGroup,
-    val isBottomAreaAvailable: Boolean
-) : Serializable
