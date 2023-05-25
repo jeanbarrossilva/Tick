d76e8a7f288aed4d49ef06ebfe51d7b20c052a46
Jean Silva <jeanbarrossilva@outlook.com>
2023-05-25 10:21:51 -0300

    change: add sample to-dos to demo flavor on first launch

--- app/src/demo/java/com/jeanbarrossilva/tick/app/demo/DemoTickApplication.kt
+++ app/src/demo/java/com/jeanbarrossilva/tick/app/demo/DemoTickApplication.kt
@@ -1,25 +1,26 @@
 package com.jeanbarrossilva.tick.app.demo
 
+import android.content.Context
+import androidx.core.content.edit
 import com.jeanbarrossilva.tick.app.TickApplication
 import com.jeanbarrossilva.tick.app.demo.extensions.addGroups
 import com.jeanbarrossilva.tick.core.todo.domain.group.ToDoGroup
 import com.jeanbarrossilva.tick.core.todo.infra.ToDoEditor
-import com.jeanbarrossilva.tick.core.todo.infra.ToDoRepository
 import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
 
 internal class DemoTickApplication : TickApplication() {
     override fun onCreate() {
         super.onCreate()
-        MainScope().launch { addSamplesOnAbsence() }
+        MainScope().launch { addSamplesOnFirstLaunch() }
     }
 
-    private suspend fun addSamplesOnAbsence() {
-        val repository by inject<ToDoRepository>()
-        val areAbsent = repository.fetch().first().isEmpty()
-        if (areAbsent) {
+    private suspend fun addSamplesOnFirstLaunch() {
+        val preferences = getSharedPreferences(PREFERENCES_FILE_NAME, Context.MODE_PRIVATE)
+        val isFirstLaunch = preferences.getBoolean(FIRST_LAUNCH_PREFERENCE_KEY, true)
+        if (isFirstLaunch) {
+            preferences.edit { putBoolean(FIRST_LAUNCH_PREFERENCE_KEY, false) }
             addSamples()
         }
     }
@@ -27,5 +28,10 @@ internal class DemoTickApplication : TickApplication() {
     private suspend fun addSamples() {
         val editor by inject<ToDoEditor>()
         editor.addGroups(ToDoGroup.samples)
+    }
+
+    companion object {
+        private const val PREFERENCES_FILE_NAME = "tick"
+        private const val FIRST_LAUNCH_PREFERENCE_KEY = "is_first_launch"
     }
 }
