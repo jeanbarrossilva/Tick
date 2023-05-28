662b70b9dc7518a0ff73fced85f5953b645f3c66
Jean Silva <jeanbarrossilva@outlook.com>
2023-05-28 14:24:20 -0300

    refact: remove remaining theme-related structures from :app

--- app/src/main/java/com/jeanbarrossilva/tick/platform/theme/overlay/ProvidableCompositionLocal.extensions.kt
+++
@@ -1,9 +0,0 @@
-package com.jeanbarrossilva.tick.platform.theme.overlay
-
-import androidx.compose.runtime.CompositionLocal
-import androidx.compose.runtime.compositionLocalOf
-
-/** [CompositionLocal] that provides [Overlays]. **/
-internal val LocalOverlays = compositionLocalOf {
-    Overlays.Unspecified
-}
--- app/src/main/java/com/jeanbarrossilva/tick/platform/theme/spacing/ProvidableCompositionLocal.extensions.kt
+++
@@ -1,9 +0,0 @@
-package com.jeanbarrossilva.tick.platform.theme.spacing
-
-import androidx.compose.runtime.CompositionLocal
-import androidx.compose.runtime.compositionLocalOf
-
-/** [CompositionLocal] that provides [Spacings]. **/
-internal val LocalSpacings = compositionLocalOf {
-    Spacings.Unspecified
-}
